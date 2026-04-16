33f6789848695d7ad65dcee162de5b083e440103
kipp <batchelork3@cardiff.ac.ukgit config --global user.email batchelork3@cardiff.ac.uk>
2026-04-16 15:56:21 +0100

    simple version check file

---
+++ tests/versions.py
@@ -0,0 +1,7 @@
+import torch, transformers, sklearn
+import sys; 
+
+print( "torch ", torch.__version__)
+print("transformers ", transformers.__version__)
+print("sklearn ", sklearn.__version__)
+print("system ", sys.version)
